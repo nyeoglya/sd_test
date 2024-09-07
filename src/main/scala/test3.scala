a43099f10d49d1291b883db04006ee5662a3f8f7
hyunseong <hyunseong@postech.ac.kr>
2024-09-07 19:39:26 +0900

    pattern matching test 1

--- src/main/scala/test3.scala
+++ src/main/scala/test3.scala
@@ -1,3 +1,10 @@
-object Test {
-    
+object PatternMatching extends App {
+    val bools = Seq(true, false)
+
+    for (bool <- bools) {
+        bool match {
+            case true => println("Got heads")
+            case false => println("Got tails")
+        }
+    }
 }
